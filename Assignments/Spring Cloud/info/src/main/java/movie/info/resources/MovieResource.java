ebd02dc04611a95eea6345848596fceda485b26c
Shirdik <shirdi1241@outlook.com>
2021-03-11 18:02:37 +0530

    Eureka Updation

--- Assignments/Spring Cloud/info/src/main/java/movie/info/resources/MovieResource.java
+++ Assignments/Spring Cloud/info/src/main/java/movie/info/resources/MovieResource.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import movie.info.models.Movie;
-
 @RestController
 @RequestMapping("/movie")
 public class MovieResource {
